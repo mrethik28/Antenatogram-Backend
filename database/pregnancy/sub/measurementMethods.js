8dd380c3290fd98336d3e04e782c4290df83ff4a
Rethik Manoharan <rethikm2003@gmail.com>
2024-12-12 18:50:58 +0530

    all

--- database/pregnancy/sub/measurementMethods.js
+++ database/pregnancy/sub/measurementMethods.js
@@ -1,20 +1,22 @@
 import { pool } from "../../db.js";
 import { DBError } from "../../../src/utils/backendError.js";
 
-export async function findAndUpdateEntry(entry) {
+export async function findAndEdit(option, entry) {
     let connection;
     try{
          connection = await pool.getConnection();
          try{
-            let query = "UPDATE measurement SET value = ? WHERE measurement_id = (?)";
-            const results = await connection.query(query, [entry.value,entry.measurement_id]);
-            console.log(results);
-            if(results.affectedRows == 1) return true;
-            return false;
+            if(option === "update"){
+                let query = "UPDATE measurement SET value = ? WHERE measurement_id = (?)";
+                const results = await connection.query(query, [entry.value,entry.measurement_id]);
+                console.log(results);
+                if(results.affectedRows == 1) return true;
+                return false;
+            }
          }
          catch(error){
             console.log(error);
-            return new DBError("Could not add refresh token to DB", error);
+            return new DBError(`could not perform ${option} on DB `, error);
          }
     }
     catch(error){
--- src/components/pregnancy/measurementServices.js
+++ src/components/pregnancy/measurementServices.js
@@ -1,25 +1,7 @@
 import { findAndUpdateEntry } from "../../../database/pregnancy/sub/measurementMethods.js";
 
-async function update(req, res, next) {
-  const records = req.body.content;
-//   for(let i=0; i<records.length; i++) {
-//     update = findAndUpdateEntry(records[i]);
-//     if(update instanceof Error) return next(update);
-// };
-  update = findAndUpdateEntry([{"measurement_id": "abs", "value" : 22 }]);
-  if(update === true ) res.send({"message": "successfully updated records"});
-}
-async function add(req, res, next) {
-  const records = req.body.content;
-  records.map((entry) => {
-    addEntry(entry);
-  });
-}
-async function remove(req, res, next) {
-  const records = req.body.content;
-  records.map((entry) => {
-    deleteEntry(entry);
-  });
+async function update(req, res, next){
+
 }
 
 export const measurementServices = { update };
